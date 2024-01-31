0d905fee84d45df836ddeefee4e7d3b7e769be06
Akhil Varma Nadimpalli <132878058+akhilvarma2378@users.noreply.github.com>
2024-01-31 21:23:47 +0000

    Variables in Javascript

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const userId = 12345;
+let userName = 'Akhil';
+var password = "Varma";
+city = "Hyderabad";
+// userId = 3; 
+
+userName = "Sankar";
+password = "Aleti";
+city = "Bangalore";
+/*
+
+prefer not to use Var 
+
+*/
+// console.log(userId);
+let state;
+
+console.table([userId,userName,password,city,state]);
