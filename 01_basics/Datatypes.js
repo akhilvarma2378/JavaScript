01bf3bbefec075b1bc2215565f1f783e77a2a54c
Akhil Varma Nadimpalli <132878058+akhilvarma2378@users.noreply.github.com>
2024-01-31 23:06:25 +0000

    DataTypes

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,33 @@
+"use strict" //treat all js as newer version. used by default in newer version runtimes,engines.
+
+// alert(3+3); //runtime(node,deno,bun js) not supports, browsers prompts inside alert
+
+console.log(3 
+    +
+     3) // code readability should be maintained ; not needed for some places.
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// DATATYPES IN JS
+// number => 2 to the power 53
+// bigInt
+// string => '' or ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+// typeof is a function that returns the datatype of value/variable/constant;
+
+console.log(typeof null); //returns object
+console.log(typeof undefined); // returns undefined
+
