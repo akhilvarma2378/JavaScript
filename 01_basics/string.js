a707b2ddfd8121788fea7e20d63007aa3e709d54
Akhil Varma Nadimpalli <132878058+akhilvarma2378@users.noreply.github.com>
2024-02-13 18:29:45 +0000

    js strings

---
+++ 01_basics/string.js
@@ -0,0 +1,18 @@
+let name = "akhil";
+let age = 20;
+
+
+// string interpolation
+console.log(`Name is : ${name.toUpperCase()} and age is ${age}`);
+
+//string declaration using objects oop
+let name2 = new String("Sankar");
+console.log(name2);
+
+console.log(name.length);
+console.log(name.substr(0,2));
+console.log(name.slice(-5,4));
+console.log(name2.replace("Sa","bhaya"));
+console.log(name2.trim());
+console.log(name.includes('hil'));
+console.log(name.split('h'));
